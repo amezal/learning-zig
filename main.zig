3d30d2e8ea2cc6e594df2b12e5d288174bd7822b
Armando Meza <amezal01@hotmail.com>
2024-05-30 22:41:52 -0600

    arrays and slices

--- main.zig
+++ main.zig
@@ -2,17 +2,9 @@ const std = @import("std");
 const User = @import("./models/user.zig").User;
 
 pub fn main() void {
-    const user: User = .{ .name = "goku", .power = 12000 };
-    var user2: User = .{ .name = "mandi", .power = 13000 };
-    user2.name = "xdxddx";
-    std.debug.print("{s}'s power level is {d}\n", .{ user.name, user.power });
-    User.diagnose(user2);
-    //user.diagnose();
-    std.debug.print("Power limit is: {d}\n", .{User.SUPER_POWER});
-}
+    const a = [4]i32{ 1, 2, 3, 4, 5 };
 
-fn add(a: i64, b: i64) i64 {
-    var sum = a;
-    sum += b;
-    return sum;
+    const b = a[1..];
+
+    std.debug.print("The number is: {d}\n", .{b[1]});
 }
