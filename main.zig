0c148e4ebf3d390ad0c2ffd1e5307d24dc07ad9b
Armando Meza <amezal01@hotmail.com>
2024-05-30 23:10:14 -0600

    const, var and array slices

--- main.zig
+++ main.zig
@@ -2,9 +2,13 @@ const std = @import("std");
 const User = @import("./models/user.zig").User;
 
 pub fn main() void {
-    const a = [4]i32{ 1, 2, 3, 4, 5 };
+    var a = [_]i32{ 1, 2, 3, 4, 5 };
+    var end: usize = 4;
+    end += 1;
+    var b = a[1..end];
+    b = b[1..];
+    // const d = c[1..];
 
-    const b = a[1..];
-
-    std.debug.print("The number is: {d}\n", .{b[1]});
+    // std.debug.print("{any}\n", .{@TypeOf(b)});
+    std.debug.print("{any}\n", .{b});
 }
