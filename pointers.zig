12a71a4e25a61496100d64b609e5a11320b1fc2e
Armando Meza <amezal01@hotmail.com>
2024-05-31 11:26:33 -0600

    single item pointers

---
+++ pointers.zig
@@ -0,0 +1,27 @@
+const std = @import("std");
+
+pub fn main() void {
+    // Single item pointer to a constant.
+    const a: u8 = 0;
+    const a_ptr = &a;
+    // a_ptr.* += 1; // cannot assign to constant
+    std.debug.print("a: {}, typeof a_ptr: {}\n", .{ a, @TypeOf(a_ptr) });
+
+    // Single item pointer to a variable.
+    var b: u8 = 0;
+    const b_ptr = &b;
+    b_ptr.* += 1; // Ok
+    std.debug.print("b: {}, typeof b_ptr: {}\n", .{ (&b).*, @TypeOf(b_ptr) });
+
+    // Both ptrs are const and can't be modified themselves.
+    // a_ptr = &b; // Cannot assign to constant
+    // b_ptr = &a; // Cannot assign to constant
+
+    var c_ptr = a_ptr;
+    c_ptr = b_ptr; // Ok
+    // var c_ptr = b_ptr;
+    // c_ptr = a_ptr;
+    // Fails because we're casting from a *const u8 (a_ptr) to a *u8 (c_ptr = b_ptr).
+    // Which means going from more restrictive to less restrictive.
+    std.debug.print("c: {}, typeof c_ptr: {}\n\n", .{ c_ptr.*, @TypeOf(c_ptr) });
+}
