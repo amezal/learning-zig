300782004d75df3072332ea63901feb6cdf3117b
Armando Meza <amezal01@hotmail.com>
2024-06-02 15:02:15 -0600

    for loops

--- control_flow.zig
+++ control_flow.zig
@@ -27,7 +27,21 @@ pub fn main() void {
 
     std.debug.print("Anniversary: {s}\n", .{anniversaryName(4)});
 
-    std.debug.print("Anniversary: {s}\n", .{arrivalTimeDesc(6, true)});
+    std.debug.print("Arrival time: {s}\n", .{arrivalTimeDesc(6, true)});
+
+    const needle: u32 = 2;
+    const haystack = [_]u32{ 1, 2, 4 };
+    var haystack2 = [_]u32{ 1, 3, 4 };
+
+    std.debug.print("Needle: {}, Haystack: {any}, Contains: {}\n", .{ needle, haystack, contains(&haystack, needle) });
+    std.debug.print("Index of needle: {any}\n", .{indexOf(&haystack, needle)});
+    std.debug.print("Arr1: {any}, Arr2: {any}, Eql: {}\n", .{ haystack, haystack2, eql(u32, &haystack, &haystack2) });
+
+    // Iterate over ranges
+    // The upper bound is exclusive: 0,1,2,3,4,5,6,7,8,9
+    for (0..10) |i| {
+        std.debug.print("{d}\n", .{i});
+    }
 }
 
 // Switch
@@ -55,4 +69,36 @@ fn arrivalTimeDesc(minutes: u16, is_late: bool) []const u8 {
             return "never";
         },
     }
+}
+
+// For loops
+
+fn contains(haystack: []const u32, needle: u32) bool {
+    for (haystack) |value| {
+        if (needle == value) {
+            return true;
+        }
+    }
+    return false;
+}
+
+// We can iterate through two sequences at once, as long as they have the same length
+fn eql(comptime T: type, a: []const T, b: []const T) bool {
+    // if they aren't the same length, they can't be equal
+    if (a.len != b.len) return false;
+
+    for (a, b) |a_elem, b_elem| {
+        if (a_elem != b_elem) return false;
+    }
+    return true;
+}
+
+// We iterate through haystack and the full range (0..)
+fn indexOf(haystack: []const u32, needle: u32) ?usize {
+    for (haystack, 0..) |value, i| {
+        if (needle == value) {
+            return i;
+        }
+    }
+    return null;
 }
